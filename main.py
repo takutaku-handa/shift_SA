843f992c37835ee77d4eaa95910780f56131a0f3
takutaku-handa <takutaku.handa27@gmail.com>
2022-12-26 14:29:26 +0900

    出勤希望度の制約のみ考慮したモデル

--- main.py
+++ main.py
@@ -8,35 +8,32 @@
 # １シフトに入る人数：（バイナリ変数の縦の和 - 1or2）^2 に係数を乗ずる
 
 # ＜ソフト制約＞ハード制約を満たしたシフトの中で、ソフト制約のペナルティ値のバランスを見て人間が恣意的に選ぶというのが無難か？
-
-
-import time
+import csv
 import numpy as np
 
 import dimod
 from neal import SimulatedAnnealingSampler
 
+MANPOWER = 8
+DAY = 31
+
 if __name__ == "__main__":
-    """  BinaryQuadraticModel(linear, quadratic, offset, vartype  """
-    bqm = dimod.BinaryQuadraticModel(
-        {'cupcakes_0,0': -2.0, 'cupcakes_0,1': -2.0, 'cupcakes_1,3': 0.0, 'cupcakes_1,2': -2.0,
-         'smoothie_0,2': -2.0, 'smoothie_0,3': 0.0, 'smoothie_0,1': -2.0, 'smoothie_0,0': -2.0,
-         'lasagna_0,1': -2.0, 'lasagna_0,2': 0.0, 'lasagna_0,0': -2.0},
-        {('cupcakes_0,1', 'cupcakes_0,0'): 4.0, ('cupcakes_1,2', 'cupcakes_0,1'): 4.0,
-         ('cupcakes_1,2', 'cupcakes_1,3'): 4.0, ('smoothie_0,2', 'cupcakes_0,1'): 4.0,
-         ('smoothie_0,3', 'smoothie_0,2'): 4.0, ('smoothie_0,1', 'cupcakes_0,0'): 4.0,
-         ('smoothie_0,1', 'cupcakes_0,1'): 8.0, ('smoothie_0,1', 'smoothie_0,2'): 4.0,
-         ('smoothie_0,1', 'smoothie_0,3'): 4.0, ('smoothie_0,0', 'cupcakes_0,0'): 8.0,
-         ('smoothie_0,0', 'smoothie_0,2'): 4.0, ('smoothie_0,0', 'smoothie_0,3'): 4.0,
-         ('smoothie_0,0', 'smoothie_0,1'): 4.0, ('lasagna_0,1', 'cupcakes_1,2'): 4.0,
-         ('lasagna_0,2', 'cupcakes_1,3'): 4.0, ('lasagna_0,2', 'cupcakes_1,2'): 8.0,
-         ('lasagna_0,2', 'lasagna_0,1'): 4.0, ('lasagna_0,0', 'lasagna_0,1'): 4.0,
-         ('lasagna_0,0', 'lasagna_0,2'): 4.0}, 8.0, 'BINARY')
+    binary = np.empty((MANPOWER, DAY))
+
+    CONST_1 = []
+    with open('shift.csv', encoding="utf-8-sig") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            CONST_1.append([int(i) for i in row])
+
+    dict_for_bqm = {}
+    for i in range(len(CONST_1)):
+        for j in range(len(CONST_1[i])):
+            dict_for_bqm["x_{0}_{1}".format(i, j)] = CONST_1[i][j]
+
+    bqm = dimod.BinaryQuadraticModel(dict_for_bqm, {}, 0, "BINARY")
 
     SA_sampler = SimulatedAnnealingSampler()
     sample_set = SA_sampler.sample(bqm, num_reads=100)
-    # SAサンプラーだと、num_ocが全部１になってしまう。(この問題は恐らくソルバー内部の問題で、根本的解決は不可能）
-
-    # 代わりに、エネルギーが小さい順に取り出し、同じのが出たことある奴なら無視するという方法なら採れる。
     order = np.argsort(sample_set.record["energy"])
     print(sample_set.record[order][:10])
