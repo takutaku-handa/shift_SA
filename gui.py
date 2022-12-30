2d9dbc6ad6d4bd9b6ba901b3dafbfaa4d02d03db
takutaku-handa <takutaku.handa27@gmail.com>
2022-12-31 02:25:54 +0900

    それなりに使えるアプリになった。 前回までのプログラムでは、BQMの変数を「x_1_18」のように命名していたが、それだとサンプルの結果を取り出すときに「x_1_1」の次が「x_1_2」ではなく「x_1_10」になり、結果の表示がおかしくなっていた。 sample関数のパラメータbeta_range, beta_schedule, num_sweepなどは勉強して改善する必要がありそう。

--- gui.py
+++ gui.py
@@ -23,36 +23,45 @@ class Application(tk.Frame):
         self.initModel()
 
         # ウィジェット配置
-        notebook = ttk.Notebook(self.master)
+        self.notebook = ttk.Notebook(self.master)
 
-        menuBar = tk.Menu()
-        self.master.config(menu=menuBar)
+        self.menubar = tk.Menu()
+        self.master.config(menu=self.menubar)
 
-        fileMenu = tk.Menu()
-        menuBar.add_cascade(label="ファイル", menu=fileMenu)
-        # fileMenu.add_command(label="ファイル選択", command=self.initModel)
+        self.menu_file = tk.Menu()
+        self.menubar.add_cascade(label="File", menu=self.menu_file)
+        self.menu_file.add_command(label="ファイル選択", command=self.initModel)
 
-        desire_tab = tk.Frame(notebook)
-        param_tab = tk.Frame(notebook)
-        result_tab = tk.Frame(notebook)
+        self.tab_desire = tk.Frame(self.notebook)
+        self.tab_param = tk.Frame(self.notebook)
+        self.tab_result = tk.Frame(self.notebook)
 
-        notebook.add(desire_tab, text="　出勤希望　")
-        notebook.add(param_tab, text="　パラメータ　")
-        notebook.add(result_tab, text="　結果　")
-        notebook.pack(expand=True, fill='both', padx=10, pady=10)
+        self.notebook.add(self.tab_desire, text="　出勤希望　")
+        self.notebook.add(self.tab_param, text="　パラメータ　")
+        self.notebook.pack(expand=True, fill='both', padx=10, pady=10)
 
-        button = tk.Button(text="▶ 【実行】", width=10, height=1, bd=0, fg="green")
-        button.place(x=900, y=10, anchor=tk.NW)
+        self.button_opt = tk.Button(text="▶ 【実行】", width=10, height=1, bd=0, fg="green", command=self.optimize)
+        self.button_opt.place(x=900, y=10, anchor=tk.NW)
 
-        # desire_tab #####################################################################
-        font_width = 1
-        font_height = 2
-        pad = 1
-        name_list = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        self.show_desire()
+        # self.tab_param は後から。
+        self.tr_res = None
 
-        f_const = tk.Frame(desire_tab, padx=10, pady=10)
-        f_const.pack(anchor=tk.W, padx=10, pady=10)
-        for i in range(1, 61):
+    def initModel(self):
+        self.model = shift_SA.ShiftAnneal()
+        self.model.setCSV(selectCSV())
+        self.model.setParam()
+
+    # tab_desireの表示（更新）
+    def show_desire(self, fw=1, fh=2, pad=1):
+        font_width = fw
+        font_height = fh
+        pad = pad
+        name_list = self.model.NAME
+
+        f_const = tk.Frame(self.tab_desire, padx=10, pady=10)
+        f_const.grid(row=0, column=0, padx=10, pady=10)
+        for i in range(1, self.model.DAY + 1):
             lb_date = tk.Label(f_const, text=str(int((i + 1) / 2)), width=font_width, height=font_height)
             lb_date.grid(row=0, column=i, padx=pad)
         const = self.model.const
@@ -65,57 +74,92 @@ class Application(tk.Frame):
                 lb_const = tk.Label(f_const, text=str(point), width=font_width, height=font_height)
                 lb_const.grid(row=r1 + 1, column=r2 + 1, padx=pad)
 
-        # param_tab #######################################################################
-        # ここは後から。
+    # tab_resultの表示（更新）
+    def show_result(self):
+        self.tr_res = ttk.Treeview(self.tab_result, height=15)
+        tmp_pena = self.model.getPenalty(self.model.sample_set.record[0][0])
+        self.tr_res["columns"] = [r for r in range(len(tmp_pena) + 1)]
+        self.tr_res["show"] = "headings"
+        heading_id = 1
+        self.tr_res.column(0, width=50)
+        self.tr_res.heading(0, text="ID")
+        for p in tmp_pena:
+            self.tr_res.column(heading_id, width=len(p[0]) * 23)
+            self.tr_res.heading(heading_id, text=p[0])
+            heading_id += 1
+        for sample_id in self.model.order:
+            sample = self.model.sample_set.record[sample_id][0]
+            penalty_list = self.model.getPenalty(sample)
+            values = [v[1] for v in penalty_list]
+            values.insert(0, sample_id)
+            self.tr_res.insert("", "end", values=values)
+        self.tr_res.grid(row=0, column=0, padx=10, pady=10)
 
-        # result_tab ######################################################################
-        font_width = 1
-        font_height = 2
-        pad = 1
-        name_list = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        vbar = ttk.Scrollbar(self.tab_result, orient="vertical", command=self.tr_res.yview)
+        self.tr_res.configure(yscrollcommand=vbar.set)
+        vbar.grid(row=0, column=1, padx=10, pady=10, sticky=tk.N + tk.S)
 
-        f_res = tk.Frame(result_tab, padx=10, pady=10)
-        f_res.pack(anchor=tk.W, padx=10, pady=10)
-        for i in range(1, 61):
-            lb_date = tk.Label(f_res, text=str(int((i + 1) / 2)), width=font_width, height=font_height)
+        self.tr_res.bind("<Double-1>", self.TrResDouble)
+
+    # ツリービューをダブルクリックして詳細表示
+    def TrResDouble(self, event):
+        selected_item = self.tr_res.selection()[0]
+        values_1 = self.tr_res.item(selected_item)['values'][0]
+        sample = self.model.sample_set.record[values_1][0]
+        self.show_sample(title="サンプル{0}".format(values_1), sample=sample)
+
+    # サンプルの表示
+    def show_sample(self, title="サンプル", sample=None, fw=1, fh=2, pad=1, row=0, column=0):
+        font_width = fw
+        font_height = fh
+        pad = pad
+        name_list = self.model.NAME
+        const = self.model.const
+
+        new_tab = tk.Frame(self.notebook)
+        self.notebook.add(new_tab, text=title)
+
+        f_sam = tk.Frame(new_tab, padx=10, pady=10)
+        f_sam.grid(row=row, column=column, padx=10, pady=10)
+        for i in range(1, self.model.DAY + 1):
+            lb_date = tk.Label(f_sam, text=str(int((i + 1) / 2)), width=font_width, height=font_height)
             lb_date.grid(row=0, column=i, padx=pad)
 
-        self.optimize()  # 今後はボタンを押したら最適化を実行するようにする。
-
-        first = self.model.sample_set.record[self.model.order][0][0]
         for m in range(self.model.MANPOWER):
-            day = first[self.model.DAY * m: self.model.DAY * m + self.model.DAY]
-            lb_name = tk.Label(f_res, text=name_list[m], width=font_width + 3, height=font_height)
+            day = sample[self.model.DAY * m: self.model.DAY * m + self.model.DAY]
+            lb_name = tk.Label(f_sam, text=name_list[m], width=font_width + 3, height=font_height)
             lb_name.grid(row=m + 1, column=0, padx=pad)
             for d in range(len(day)):
                 point = day[d]
                 if not point:
-                    lb_res = tk.Label(f_res, text="", width=font_width, height=font_height, relief="solid",
+                    lb_sam = tk.Label(f_sam, text="", width=font_width, height=font_height, relief="solid",
                                       borderwidth=1)
                 else:
                     if const[m][d] == 0:
-                        lb_res = tk.Label(f_res, text="", width=font_width, height=font_height, relief="solid",
+                        lb_sam = tk.Label(f_sam, text="", width=font_width, height=font_height, relief="solid",
                                           borderwidth=1, background="green")
                     elif const[m][d] == 1:
-                        lb_res = tk.Label(f_res, text="", width=font_width, height=font_height, relief="solid",
+                        lb_sam = tk.Label(f_sam, text="", width=font_width, height=font_height, relief="solid",
                                           borderwidth=1, background="yellow")
                     elif const[m][d] == 2:
-                        lb_res = tk.Label(f_res, text="", width=font_width, height=font_height, relief="solid",
+                        lb_sam = tk.Label(f_sam, text="", width=font_width, height=font_height, relief="solid",
                                           borderwidth=1, background="orange")
                     else:
-                        lb_res = tk.Label(f_res, text="", width=font_width, height=font_height, relief="solid",
+                        lb_sam = tk.Label(f_sam, text="", width=font_width, height=font_height, relief="solid",
                                           borderwidth=1, background="red")
-                lb_res.grid(row=m + 1, column=d + 1, padx=pad)
+                lb_sam.grid(row=m + 1, column=d + 1, padx=pad)
 
-    def initModel(self):
-        self.model = shift_SA.ShiftAnneal()
-        self.model.setCSV(selectCSV())
-        self.model.setParam()
+        self.notebook.select(new_tab)
 
-    # 最適化ボタン
+    # 最適化
     def optimize(self):
         self.model.setConst()
         self.model.sample()
+        self.notebook.add(self.tab_result, text="　結果リスト　")
+        ind = 0
+        # self.show_sample(sample=self.model.sample_set.record[self.model.order][0][0], column=0)
+        self.show_result()
+        self.notebook.select(self.tab_result)
 
 
 if __name__ == "__main__":
--- shift_SA.py
+++ shift_SA.py
@@ -1,4 +1,6 @@
 import csv
+import math
+
 import numpy as np
 
 import dimod
@@ -10,8 +12,7 @@ from dwave.system.samplers import DWaveSampler
 
 class ShiftAnneal:
     def __init__(self):
-        self.const = []
-
+        self.NAME = ["A", "B", "C", "D", "E", "F", "G", "H"]
         self.MANPOWER = 0
         self.DAY = 0
         self.DESIRE_CONST = 0
@@ -22,10 +23,14 @@ class ShiftAnneal:
         self.WORKDAY_CONST = 0
         self.NUM_READS = 0
 
+        self.const = []
         self.liner = {}
         self.quadratic = {}
         self.sample_set = None
         self.order = None
+
+    def getID(self, m, d):
+        return self.DAY * m + d + 100000000
 
     def setCSV(self, filename):
         with open(filename, encoding="utf-8-sig") as f:
@@ -36,84 +41,83 @@ class ShiftAnneal:
     def setParam(self):
         self.MANPOWER = 8
         self.DAY = 60
-        self.DESIRE_CONST = 9
-        self.SEQ_CONST = 500
-        self.SHIFT_SIZE_CONST = 5000
+
+        self.DESIRE_CONST = 30
+        self.SEQ_CONST = 150
+        self.SHIFT_SIZE_CONST = 150
         self.SHIFT_SIZE_LIMIT = 1
         self.WORKDAY = [7, 7, 7, 7, 7, 7, 7, 7]
-        self.WORKDAY_CONST = 10
+        self.WORKDAY_CONST = 5
+
         self.NUM_READS = 100
 
     def setConst(self):
         # １次
         for i in range(self.MANPOWER):
             for j in range(self.DAY):
-                liner_const = (self.const[i][j] ^ self.DESIRE_CONST)  # 出勤希望度による
+                liner_const = (self.const[i][j] * self.DESIRE_CONST)  # 出勤希望度による
                 liner_const += - (2 * self.SHIFT_SIZE_LIMIT) * self.SHIFT_SIZE_CONST  # １シフトに入る人数制約による
-                # liner_const += - (2 * self.WORKDAY[i]) * self.WORKDAY_CONST  # 勤務日数希望による
+                liner_const += - (2 * self.WORKDAY[i]) * self.WORKDAY_CONST  # 勤務日数希望による
+                key = "x_{0}".format(self.getID(i, j))
                 try:
-                    self.liner["x_{0}_{1}".format(i, j)] += liner_const
+                    self.liner[key] += liner_const
                 except KeyError:
-                    self.liner["x_{0}_{1}".format(i, j)] = liner_const
+                    self.liner[key] = liner_const
 
         # ２次
         # 昼夜連勤の禁止による
         for i in range(self.MANPOWER):
             for j in range(int(self.DAY / 2)):
                 j *= 2
+                key = ("x_{0}".format(self.getID(i, j)), "x_{0}".format(self.getID(i, j + 1)))
                 try:
-                    self.quadratic[("x_{0}_{1}".format(i, j), "x_{0}_{1}".format(i, j + 1))] += self.SEQ_CONST
+                    self.quadratic[key] += self.SEQ_CONST
                 except KeyError:
-                    self.quadratic[("x_{0}_{1}".format(i, j), "x_{0}_{1}".format(i, j + 1))] = self.SEQ_CONST
+                    self.quadratic[key] = self.SEQ_CONST
 
         # １シフトに入る人数制約による
         for i1 in range(self.MANPOWER):
             for i2 in range(self.MANPOWER):
                 if i1 == i2:
                     for j in range(self.DAY):
+                        key = ("x_{0}".format(self.getID(i1, j)), "x_{0}".format(self.getID(i2, j)))
                         try:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i1, j), "x_{0}_{1}".format(i2, j))] += 1 * self.SHIFT_SIZE_CONST
+                            self.quadratic[key] += 1 * self.SHIFT_SIZE_CONST
                         except KeyError:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i1, j), "x_{0}_{1}".format(i2, j))] = 1 * self.SHIFT_SIZE_CONST
+                            self.quadratic[key] = 1 * self.SHIFT_SIZE_CONST
                 else:
                     for j in range(self.DAY):
+                        key = ("x_{0}".format(self.getID(i1, j)), "x_{0}".format(self.getID(i2, j)))
                         try:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i1, j), "x_{0}_{1}".format(i2, j))] += 2 * self.SHIFT_SIZE_CONST
+                            self.quadratic[key] += 2 * self.SHIFT_SIZE_CONST
                         except KeyError:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i1, j), "x_{0}_{1}".format(i2, j))] = 2 * self.SHIFT_SIZE_CONST
+                            self.quadratic[key] = 2 * self.SHIFT_SIZE_CONST
 
-        # 勤務日数希望による(いったん無視する)
-        """
+        # 勤務日数希望による
         for j1 in range(self.DAY):
             for j2 in range(self.DAY):
                 if j1 == j2:
                     for i in range(self.MANPOWER):
+                        key = ("x_{0}".format(self.getID(i, j1)), "x_{0}".format(self.getID(i, j2)))
                         try:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i, j1), "x_{0}_{1}".format(i, j2))] += 1 * self.WORKDAY_CONST
+                            self.quadratic[key] += 1 * self.WORKDAY_CONST
                         except KeyError:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i, j1), "x_{0}_{1}".format(i, j2))] = 1 * self.WORKDAY_CONST
+                            self.quadratic[key] = 1 * self.WORKDAY_CONST
                 else:
                     for i in range(self.MANPOWER):
+                        key = ("x_{0}".format(self.getID(i, j1)), "x_{0}".format(self.getID(i, j2)))
                         try:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i, j1), "x_{0}_{1}".format(i, j2))] += 2 * self.WORKDAY_CONST
+                            self.quadratic[key] += 2 * self.WORKDAY_CONST
                         except KeyError:
-                            self.quadratic[
-                                ("x_{0}_{1}".format(i, j1), "x_{0}_{1}".format(i, j2))] = 2 * self.WORKDAY_CONST
-        """
+                            self.quadratic[key] = 2 * self.WORKDAY_CONST
 
     def sample(self):
         # BQMモデルに変換
         bqm = dimod.BinaryQuadraticModel(self.liner, self.quadratic, 0, "BINARY")
         # サンプリング
         SA_sampler = SimulatedAnnealingSampler()
-        self.sample_set = SA_sampler.sample(bqm, num_reads=self.NUM_READS)
+        self.sample_set = SA_sampler.sample(bqm, num_reads=self.NUM_READS, beta_schedule_type="geometric",
+                                            num_sweeps_per_beta=100, num_sweeps=10000)
         self.order = np.argsort(self.sample_set.record["energy"])
 
     # 面倒なので、量子アニーラはいったん考えない。
@@ -132,8 +136,64 @@ class ShiftAnneal:
     def outputCSV(self):
         pass
 
-    def getPenalty(self):
-        pass
+    def getPenalty(self, sample):
+        # ペナルティ：出勤希望度違反数、昼夜連勤数、人数超過日数、人数不足日数、勤務日のばらつき、勤務日数のばらつき
+        pena_desire = [0, 0, 0, 0]
+        pena_seq = 0
+        pena_over = 0
+        pena_lack = 0
+        pena_dist = 0
+        pena_workday = 0
+
+        count_horizontal = [0 for i in range(self.MANPOWER)]
+        count_vertical = [0 for i in range(self.DAY)]
+        max_seq_work = 0
+        max_seq_off = 0
+
+        index = 0
+        for m in range(self.MANPOWER):
+            work_date = []
+            date = 1
+            seq_work = 0
+            seq_off = 0
+            for d in range(self.DAY):
+                s = sample[index]
+                if s:
+                    work_date.append(date)
+                    seq_work += 1
+                    seq_off = 0
+                else:
+                    seq_work = 0
+                    seq_off += 1
+                if seq_work > max_seq_work:
+                    max_seq_work = seq_work
+                if seq_off > max_seq_off:
+                    max_seq_off = seq_off
+                date += 1
+                pena_desire[self.const[m][d]] += s
+                if (d % self.DAY) % 2 == 0 and d < self.DAY and s * sample[index + 1]:
+                    pena_seq += 1
+                count_horizontal[m] += s
+                count_vertical[d] += s
+                index += 1
+            pena_dist += math.sqrt(np.var(work_date))
+
+        for cv in count_vertical:
+            if cv > 1:
+                pena_over += 1
+            if cv < 1:
+                pena_lack += 1
+
+        for m in range(self.MANPOWER):
+            gap = (self.WORKDAY[m] - count_horizontal[m]) * (self.WORKDAY[m] - count_horizontal[m])
+            pena_workday += gap
+
+        ret = [("0", pena_desire[0]), ("1", pena_desire[1]), ("2", pena_desire[2]), ("3", pena_desire[3]),
+               ("昼夜連勤", pena_seq), ("人数超過", pena_over), ("人数不足", pena_lack),
+               ("ばらけ具合", int(pena_dist)), ("勤務日数希望違反", pena_workday),
+               ("最大連勤", max_seq_work), ("最大連休", max_seq_off)]
+
+        return ret
 
     def calcPenalty(self):
         pass
